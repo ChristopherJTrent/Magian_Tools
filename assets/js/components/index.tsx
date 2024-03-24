68d4abe98724f278323819686067f3ecb9c920b6
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-03-23 21:30:40 -0700

    scaffold redux store

--- assets/js/components/index.tsx
+++ assets/js/components/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Index:React.FC = () => {
     return (
-        <h1>Hello, World from React on Typescript with Phoenix on Elixir</h1>
+        <h1>Hello, World from React.ts driven by Phoenix on Elixir</h1>
     )
 }
